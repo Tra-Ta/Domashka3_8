77e27fd522e24fa9f207e16fcf302ac08a8376aa
Tra-Ta <madtazik@gmail.com>
2022-12-09 23:31:46 +0300

    Complete Task066

--- Task066/Program.cs
+++ Task066/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите два числа:");
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m < 0) Console.WriteLine("Number M not natural");
+else if (n < 0) Console.WriteLine("Number N not natural");
+else
+{
+    SumNaturalNumbers(m, n);
+
+    int SumNaturalNumbers(int numM, int numN)
+    {
+        if (numN == numM) return numN;
+        return numN + SumNaturalNumbers(numM, numN - 1);
+
+    }
+
+    int res = SumNaturalNumbers(m, n);
+    Console.WriteLine(res);
+}
